ed480da648ab26c3b1576909fec530d52681f012
jesusQuintana-30-2017 <32751057+jesusQuintana-30-2017@users.noreply.github.com>
2023-06-30 17:53:49 -0600

    jesus quintana
    jesus quintana

--- src/main/java/com/example/examen/controllers/MainController.java
+++ src/main/java/com/example/examen/controllers/MainController.java
@@ -4,13 +4,13 @@ import com.example.examen.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Controller
-@RequestMapping("/examen")
+@CrossOrigin(origins = "*", methods = {RequestMethod.POST})
+@EnableWebMvc
+@RestController
+@RequestMapping("/miruta")
 public class MainController {
 
     @Autowired
